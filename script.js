ea14d74025c5d8c312e6ff0876d4cd0bf8746d6e
Victoria Bright <victoriacbright92@gmail.com>
2022-10-19 00:30:10 -0500

    not complete, need to go over

---
+++ script.js
@@ -0,0 +1,60 @@
+//1
+
+for (i = 1; i < 7; ++i) {
+    console.log(i);
+};
+
+//2 
+
+for (i = 5; i < 25; i +=4) {
+    console.log(i);
+};
+
+//3a
+
+const wizards = ['Harry Potter', 'Hermione Granger', 'Ron Weasley'];
+
+//3b
+
+for (i = 0; i < wizards.length; i++) {
+    console.log(wizards[i]);
+};
+
+//4a
+
+let harryPotterMovies = 0;
+
+//4b
+
+while (harryPotterMovies < 8) {
+    harryPotterMovies++;
+}
+
+//4c
+
+console.log(harryPotterMovies);
+
+//BONUS
+//5a
+const hogwartsHouses = [`Gryffindor`, `Hufflepuff`, `Ravenclaw`, `Slytherin`];
+
+//5b
+//come back to this
+for (houses of hogwartsHouses) {
+    for (house of hogwartsHouses) {
+        console.log(house);
+    }
+};
+
+//6a
+
+const quote = [`Yer`, `A`, `Wizard`, `Harry`];
+
+//6b
+
+//come back to this
+
+
+//7
+
+//come back to this
